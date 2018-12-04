cde843830d06d30635503420ae5c1b660dc7f551
Raminta-Zurauskaite <raminta.zurauskaite@stud.vgtu.lt>
2018-12-04 14:40:29 +0200

    Basic form

--- atsparumosalciuiskaiciuokle.client/src/App.js
+++ atsparumosalciuiskaiciuokle.client/src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div  id="myDIV" className="App">
+          <div className="header">Eksploatacinio atsparumo šalčiui skaičiuoklė</div>
+          <form>
+            <div>
+              <label>m0 g:</label>
+              <input type="text"/>
+              <label>hmax mm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m1 g:</label>
+              <input type="text"/>
+              <label>aa cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m2 g:</label>
+              <input type="text"/>
+              <label>av cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m3 g:</label>
+              <input type="text"/>
+              <label>ba cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m4 g:</label>
+              <input type="text"/>
+              <label>bv cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m5 g:</label>
+              <input type="text"/>
+              <label>h1 cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>m6 g:</label>
+              <input type="text"/>
+              <label>h2 cm:</label>
+              <input type="text"/>
+            </div>
+            <div>
+              <label>hmin mm:</label>
+              <input type="text"/>
+            </div>
+          <button>Skaiciuoti</button>
+
+          <div>
+          Rezultatai placeholder.
+          </div>
+  
+          </form>
       </div>
     );
   }
 }
+
+
 
 export default App;
